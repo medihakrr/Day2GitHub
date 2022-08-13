32e991681cb2b64b67ebf38faa2eac47e0dec966
Mediha Kurar <medihakurar@gmail.com>
2022-08-13 23:19:29 +0300

    son hali

---
+++ src/Day1Git.java
@@ -0,0 +1,7 @@
+public class Day1Git {
+    public static void main(String[] args) {
+        System.out.println("github 1.denemem");
+        System.out.println("2.denemem");
+        System.out.println("3.denemem");
+    }
+}
---
+++ src/Day2Git2.java
@@ -0,0 +1,5 @@
+public class Day2Git2 {
+    public static void main(String[] args) {
+        System.out.println("git 2.class");
+    }
+}
